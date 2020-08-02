007b2ed7a5a910f3ab00c892792ee997a09913d5
taqnar <muzammilthottathil.official@gmail.com>
2020-08-02 10:32:43 +0530

    updating

---
+++ js/appDummy.js
@@ -0,0 +1,56 @@
+const hamburger = () => {
+  var element = document.getElementById("hamburger");
+  var navList = document.querySelector("#mobile-nav");
+  var registration = document.querySelector(".alumni-registration");
+  var content = document.querySelector(".content");
+
+  navList.classList.toggle("nav-animate");
+  element.classList.toggle("is-active");
+  registration.classList.toggle("blur");
+  content.classList.toggle("blur");
+};
+const hamburger1 = () => {
+  var element = document.getElementById("hamburger");
+  var navList = document.querySelector("#mobile-nav");
+
+  navList.classList.toggle("nav-animate");
+  element.classList.toggle("is-active");
+};
+
+document.querySelector(".home-page").style.display = "none";
+document.querySelector(".home-page").classList.add("js-loading");
+
+window.addEventListener("load", function () {
+  document
+    .querySelector(".loader-wrapper")
+    .classList.add("loader-wrapper-remove");
+  document.querySelector(".home-page").style.display = "inline";
+  document.querySelector(".home-page").classList.remove("js-loading");
+});
+
+const redirect = (pageName) => {
+  if (pageName === "alumni-portal") {
+    window.location = "./pages/alumni-portal.html";
+  } else if (pageName === "home") {
+    window.location = "./index.html";
+  } else {
+    window.location.hash = pageName;
+  }
+  console.log(pageName);
+};
+const redirect1 = (pageName) => {
+  if (pageName === "home") {
+    window.location = "../index.html";
+  } else {
+    window.location.hash = pageName;
+  }
+  console.log(pageName);
+};
+
+AOS.init({
+  duration: 1200,
+});
+
+const register = () => {
+  console.log("register");
+};
